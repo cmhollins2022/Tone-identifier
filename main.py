ae5bfdb3f72fba43687c3532cd3d92ea353eb6c6
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-04-11 22:04:12 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# Task: In the simplest and most efficient way possible, create a program that takes in a string of words, or
+# that can read a file, then using either a given list of words (for each tone), or a machine learning algorithm,
+# dictate the tone of the given passage.
+
+# Practicality:
+
+#: Possible Use Cases:
