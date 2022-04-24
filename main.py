6a55b743eb017f7d3deda91b14f005ac05c221ff
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-04-23 22:47:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -39,3 +39,14 @@ read_and_store_text("sample-text.txt")
 # then, an algorithm could query the API to see if any of the 5 (or more, depending on how accurate
 # or specific we would like to be) can check to see if the words are contained in the
 # "Synonym" array or list of words (for the given word within the API)
+
+For front end, you could even have an option to possibly give a definition of the word when clicked.
+def identify_tone_of_word(word: str):
+    thesaurus_api = requests.get(
+        f"https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{word}"
+        f"?key=457b0e9d-88b8-470d-b118-a7d41baa1d92").json()
+
+    return print(json.dumps(thesaurus_api, indent=1))
+
+
+identify_tone_of_word("success")
