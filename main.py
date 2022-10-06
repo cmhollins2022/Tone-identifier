cfdd1917567cdc0b717cfe316d3b6918e02543b8
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-10-05 21:33:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,25 @@ import requests
 
 # Practicality: This could be a good way to quickly identify the tone and overall feel of someone's writing. The use
 # wouldn't necessarily have to stop there. It could be used to (possibly) suggest replacements for words. A
-# functionality similar to the "Grammarly" application. Or perhaps an application that can even dictate the emotion of the expression on someones's face?
-# Interesting...
+# functionality similar to the "Grammarly" application.
 
 # Pseudocode: First Steps
 
-# Step 1: After a file or list of words is entered, break up the words, and enter them into a list. Do not include
+# 1 - After a file or list of words is entered, break up the words, and enter them into a list. Do not include
 # any form of punctuation. The list should be purely made up of the words from the text.
+
+# 2 - Define "Positive" and "Negative" somehow...
+
 
 # Main file function that allows words to be broken up into individual strings,
 # remove punctuation, then appended to a list:
+
+# Querying the dictionary API... going to explore more strategies and options soon.
+# Idea: Having 5 synonyms of each word both positive and negative... within a list...
+# then, an algorithm could query the API to see if any of the 5 (or more, depending on how accurate
+# or specific we would like to be) can check to see if the words are contained in the
+# "Synonym" array or list of words (for the given word within the API)
+# For front end, you could even have an option to possibly give a definition of the word when clicked.
 
 # RegEx strategy:
 
@@ -36,24 +45,12 @@ def read_and_store_text(file_name):
 read_and_store_text("sample-text.txt")
 
 
-# Querying the dictionary API... going to explore more strategies and options soon.
-# Idea: Having 5 synonyms of each word both positive and negative... within a list...
-# then, an algorithm could query the API to see if any of the 5 (or more, depending on how accurate
-# or specific we would like to be) can check to see if the words are contained in the
-# "Synonym" array or list of words (for the given word within the API)
-
-# For front end, you could even have an option to possibly give a definition of the word when clicked.
-def identify_tone_of_word(word: str):
-    thesaurus_api = (requests.get(
-        f"https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{word}"
-        f"?key=457b0e9d-88b8-470d-b118-a7d41baa1d92")).json()
-    data = json.load(thesaurus_api)
+def synonyms_list(word: str):
     new_list = []
-    for items in data['meta']['syns']:
-        synonyms = items[2]
-        new_list.append(synonyms)
+    thesaurus_api_data = requests.get(
+        f"https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{word}"
+        f"?key=457b0e9d-88b8-470d-b118-a7d41baa1d92").json()
+    print(thesaurus_api_data['meta'])
 
-    return print(new_list)
 
-
-identify_tone_of_word("success")
+synonyms_list("happy")
